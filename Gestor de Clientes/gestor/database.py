67e99d385d187b3ce8211adefe87ce91988169b0
lauralardies <lrodrlar@myuax.com>
2022-10-03 08:36:40 +0200

    organización y mock database

---
+++ Gestor de Clientes/gestor/database.py
@@ -0,0 +1,40 @@
+class Cliente:
+
+    def __init__(self, dni, nombre, apellido):
+        self.dni = dni
+        self.nombre = nombre
+        self.apellido = apellido
+
+    def __str__(self):
+        return f"({self.dni}) {self.nombre} {self.apellido}"
+
+class Clientes:
+
+    lista = [] # Lista de clientes
+    
+    @staticmethod
+    def buscar(dni):
+        for cliente in Clientes.lista:
+            if Cliente.dni == dni:
+                return cliente
+    
+    @staticmethod
+    def crear(dni, nombre, apellido):
+        cliente = Cliente(dni, nombre, apellido)
+        Clientes.lista.append(cliente)
+        return cliente
+
+    @staticmethod
+    def modificar(dni, nombre, apellido):
+        for i, cliente in enumerate(Clientes.lista):
+            if Cliente.dni == dni:
+                Clientes.lista[i].nombre = nombre
+                Clientes.lista[i].apellido = apellido
+                return Clientes.lista[i]
+    
+    @staticmethod
+    def borrar(dni):
+        for i, cliente in enumerate(Clientes.lista):
+            if Cliente.dni == dni:
+                cliente = Clientes.lista.pop(i)
+                return cliente
