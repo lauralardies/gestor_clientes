d6ad19d5d18c75794837fc427663653ebe3639e9
lauralardies <lrodrlar@myuax.com>
2022-10-05 10:38:32 +0200

    intentos

--- Gestor de Clientes/gestor/menu.py
+++ Gestor de Clientes/gestor/menu.py
@@ -1,9 +1,10 @@
 import helpers
-import database as db
+import database as db 
 
 def iniciar():
     while True:
         helpers.limpiar_pantalla()
+
         print("========================")
         print(" BIENVENIDO AL Manager ")
         print("========================")
@@ -22,7 +23,7 @@ def iniciar():
             print("Listando los clientes...\n")
             for cliente in db.Clientes.lista:
                 print(cliente)
-
+            
         if opcion == '2':
             print("Buscando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
@@ -31,10 +32,13 @@ def iniciar():
 
         if opcion == '3':
             print("Añadiendo un cliente...\n")
+
             while 1:
                 dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1char)").upper()
                 if helpers.dni_valido(dni, db.Clientes.lista):
                     break
+
+            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
             nombre = helpers.leer_texto(2, 30, "Nombre (de 2 a 30 chars)").capitalize()
             apellido = helpers.leer_texto(2, 30, "Apellido (de 2 a 30 chars)").capitalize()
             db.Clientes.crear(dni, nombre, apellido)
@@ -42,6 +46,7 @@ def iniciar():
 
         if opcion == '4':
             print("Modificando un cliente...\n")
+            dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
             cliente = db.Clientes.buscar(dni)
             if cliente:
                 nombre = helpers.leer_texto(2, 30, f"Nombre (de 2 a 30 chars)[{cliente.nombre}]").capitalize()
@@ -50,7 +55,7 @@ def iniciar():
                 print("Cliente modificado correctamente.")
             else:
                 print("Cliente no encontrado.")
-
+            
         if opcion == '5':
             print("Borrando un cliente...\n")
             dni = helpers.leer_texto(3, 3, "DNI (2 ints y 1 char)").upper()
